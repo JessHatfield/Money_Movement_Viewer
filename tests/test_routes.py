c357259c46ce7ffa5f2cc3c0b01f6861742ae5d7
josh <jh@semetrical.com>
2022-06-03 19:09:36 +0100

    Added integration test for money movement view

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,8 +18,6 @@ def generate_dummy_data():
     money_movement_1 = MoneyMovement.create(sender=person_2, receiver=person_3, money_amount=Decimal(0.0010),
                                             currency_code="GBP")
 
-
-
     money_movement_2 = MoneyMovement.create(sender=person_2, receiver=person_1, money_amount=1000.15,
                                             currency_code="GBP")
 
@@ -33,7 +31,6 @@ def generate_dummy_data():
     money_movement_2.modified_at_datetime_utc = datetime.datetime.strptime("2022-06-03 17:17:05", "%Y-%m-%d %H:%M:%S")
     money_movement_3.modified_at_datetime_utc = datetime.datetime.strptime("2022-06-03 17:17:05", "%Y-%m-%d %H:%M:%S")
     money_movement_4.modified_at_datetime_utc = datetime.datetime.strptime("2022-06-03 17:17:05", "%Y-%m-%d %H:%M:%S")
-
 
     db.session.add_all([money_movement_1, money_movement_2, money_movement_3, money_movement_4])
     db.session.commit()
@@ -141,7 +138,6 @@ class TestViewAllMoneyMovements(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-
     def test_money_movements_viewable_when_logged_in(self):
         new_user = User.create(email="joshuahatfield.jh@gmail.com", name="Josh", password="password")
 
@@ -160,12 +156,22 @@ class TestViewAllMoneyMovements(unittest.TestCase):
         # I'm not a fan of testing against exact HTML code in an integration test. We are testing implementation, but really we just want to test behaviours
         # In these tests we only really care that a row exists containing the movement data. We don't care about css/exact html tags used
         # Testing against implementation details can make tests more brittle, easier to break and reduce the ease at which changes can be made
-
+        # Given more time BS4 or selenium could prehaps be used here and we could search by Roles or classes (like forms/buttons or table_rows). Similar to the React Testing Library
+        # The assertions below are hard to read and a little brittle but still serve their purpose of confirming outputs meet expectations.
 
         # Check each of the table rows contains expected money movement data
-        self.assertIn("""<tr id=2>\n                <td>2022-06-03 17:17:05</td>\n                <td>1000.1500 GBP</td>\n                <td>Jeffery Bezos</td>\n                <td><a href="/view_money_movement/2">Details</a></td>\n            </tr>\n""",money_movements_table_page.text)
-
-        print()
+        self.assertIn(
+            """<tr id=2>\n                <td>2022-06-03 17:17:05</td>\n                <td>1000.1500 GBP</td>\n                <td>Jeffery Bezos</td>\n                <td><a href="/view_money_movement/2">Details</a></td>\n            </tr>\n""",
+            money_movements_table_page.text)
+        self.assertIn(
+            """<tr id=1>\n                <td>2022-06-03 17:17:05</td>\n                <td>0.0010 GBP</td>\n                <td>Jeffery Bezos</td>\n                <td><a href="/view_money_movement/1">Details</a></td>\n            </tr>\n""",
+            money_movements_table_page.text)
+        self.assertIn(
+            """<tr id=3>\n                <td>2022-06-03 17:17:05</td>\n                <td>1000.0000 GBP</td>\n                <td>Clément Pergaud</td>\n                <td><a href="/view_money_movement/3">Details</a></td>\n            </tr>\n""",
+            money_movements_table_page.text)
+        self.assertIn(
+            """<tr id=4>\n                <td>2022-06-03 17:17:05</td>\n                <td>1000.0000 EUR</td>\n                <td>Emma Smith</td>\n                <td><a href="/view_money_movement/4">Details</a></td>\n            </tr>\n""",
+            money_movements_table_page.text)
 
 
 if __name__ == '__main__':
